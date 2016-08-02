fce5b91d0956671a7a6eccafd35adf5e500c8b06
JohnBesh <johnbesh91@gmail.com>
2016-08-03 00:45:51 +0200

    Make coding style consistent

--- 75_prototypes_write/6-check_if_odd_number.c
+++ 75_prototypes_write/6-check_if_odd_number.c
@@ -1,17 +1,14 @@
 #include "my_functions.h"
 
 /* Using bit operators, this function returns 'O' if a number is odd and 'E' if number is even */
-
 char odd(int n)
 {
   int i;
   i = 1;
-  if (n & i) /* Bit operator to compare if n & i agree. If they do, number is odd */
-    {
+  if (n & i) { /* Bit operator to compare if n & i agree. If they do, number is odd */
       return ('O');
     }
-  else
-    {
+  else {
       return ('E');
     }
 }
